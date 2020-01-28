8a9aa62d8526f351fdf9c53f71d2f8b56c816d5b
Meghan Flynn <mflynn333@gmail.com>
2020-01-28 01:59:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,6 +15,7 @@ def get_page
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+      binding.pry
     end
   end
   
